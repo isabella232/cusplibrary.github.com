074835632b193ae88ac2b4e9521ad1806a15374b
steve <sdalton1@gmail.com>
2015-01-11 17:18:21 -0800

    Updated IO docs

--- search/all_b.js
+++ search/all_b.js
@@ -6,5 +6,6 @@ var searchData=
   ['linear_5foperator_3camg_5fcontainer_3c_20indextype_2c_20valuetype_2c_20memoryspace_20_3e_3a_3asolve_5ftype_3a_3avalue_5ftype_2c_20amg_5fcontainer_3c_20indextype_2c_20valuetype_2c_20memoryspace_20_3e_3a_3asolve_5ftype_3a_3amemory_5fspace_20_3e',['linear_operator&lt;amg_container&lt; IndexType, ValueType, MemorySpace &gt;::solve_type::value_type, amg_container&lt; IndexType, ValueType, MemorySpace &gt;::solve_type::memory_space &gt;',['../classcusp_1_1linear__operator.html',1,'cusp']]],
   ['linear_5foperator_3cmatrixtype_3a_3avalue_5ftype_2c_20matrixtype_3a_3amemory_5fspace_20_3e',['linear_operator&lt;MatrixType::value_type, MatrixType::memory_space &gt;',['../classcusp_1_1linear__operator.html',1,'cusp']]],
   ['linear_5foperator_3cvaluetype_2c_20memoryspace_20_3e',['linear_operator&lt;ValueType, MemorySpace &gt;',['../classcusp_1_1linear__operator.html',1,'cusp']]],
-  ['lobpcg',['lobpcg',['../group__eigensolvers.html#ga1fa0efeaca1603b27f6f0f7dbf3de725',1,'cusp::eigen']]]
+  ['lobpcg',['lobpcg',['../group__eigensolvers.html#ga1fa0efeaca1603b27f6f0f7dbf3de725',1,'cusp::eigen']]],
+  ['lobpcg_2eh',['lobpcg.h',['../lobpcg_8h.html',1,'']]]
 ];
--- search/files_9.js
+++ search/files_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['lanczos_2eh',['lanczos.h',['../lanczos_8h.html',1,'']]],
-  ['linear_5foperator_2eh',['linear_operator.h',['../linear__operator_8h.html',1,'']]]
+  ['linear_5foperator_2eh',['linear_operator.h',['../linear__operator_8h.html',1,'']]],
+  ['lobpcg_2eh',['lobpcg.h',['../lobpcg_8h.html',1,'']]]
 ];
