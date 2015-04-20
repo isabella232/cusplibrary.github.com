17dd264e9bcc07a8dd9345592437bb735a1d6bd0
steve <sdalton1@gmail.com>
2015-04-19 22:04:48 -0700

    Fixed CHANGELOG links

--- search/all_5.js
+++ search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['fill',['fill',['../group__blas.html#ga47e816f52a6ec5f05e60a6b4f6c6ca1a',1,'cusp::blas']]],
+  ['fill',['fill',['../group__blas.html#ga9c9598eeed4b41ce91040f8a13ae3bb9',1,'cusp::blas']]],
   ['finished',['finished',['../group__monitors.html#gadca75968e0b5c62b038b122bdc07b8e0',1,'cusp::monitor']]],
   ['format_5futils_2eh',['format_utils.h',['../format__utils_8h.html',1,'']]],
   ['front',['front',['../classcusp_1_1array1d__view.html#a6c9352efdf45e36ecec6113e306a814f',1,'cusp::array1d_view']]]
--- search/variables_3.js
+++ search/variables_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['indices',['indices',['../classcusp_1_1coo__matrix__view.html#a982ecb91c2a9495e6b0a0cc37e5d9eb0',1,'cusp::coo_matrix_view']]],
+  ['indices',['indices',['../classcusp_1_1coo__matrix__view.html#a8e90b6c84cedf2517604009223d5ba21',1,'cusp::coo_matrix_view']]],
   ['invalid_5findex',['invalid_index',['../classcusp_1_1ell__matrix.html#a069ae1f6d1929685331ac42d6100d842',1,'cusp::ell_matrix::invalid_index()'],['../classcusp_1_1ell__matrix__view.html#a7d3961aa458d7f73dd625798e39bf181',1,'cusp::ell_matrix_view::invalid_index()']]]
 ];
