35e1b2b1e644dad6bd4687396389e3164ecced6e
steve <sdalton1@gmail.com>
2015-09-21 11:51:34 -0700

    Updated utility grouping

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['gallery',['Gallery',['../group__gallery.html',1,'']]],
   ['gauss_5fseidel',['gauss_seidel',['../classcusp_1_1relaxation_1_1gauss__seidel.html',1,'cusp::relaxation']]],
   ['gauss_5fseidel',['gauss_seidel',['../classcusp_1_1relaxation_1_1gauss__seidel.html#a9db23e1480f64859d4fbb11b43db0fd0',1,'cusp::relaxation::gauss_seidel::gauss_seidel(void)'],['../classcusp_1_1relaxation_1_1gauss__seidel.html#a9ac209317083f6f9585438db15be11c9',1,'cusp::relaxation::gauss_seidel::gauss_seidel(const MatrixType &amp;A, sweep default_direction=SYMMETRIC, typename thrust::detail::enable_if_convertible&lt; typename MatrixType::format, cusp::csr_format &gt;::type *=0)'],['../classcusp_1_1relaxation_1_1gauss__seidel.html#a5d13af761b064e79d1a73712f21c40ca',1,'cusp::relaxation::gauss_seidel::gauss_seidel(const gauss_seidel&lt; ValueType, MemorySpace2 &gt; &amp;A)']]],
   ['gauss_5fseidel_2eh',['gauss_seidel.h',['../gauss__seidel_8h.html',1,'']]],
@@ -17,5 +18,7 @@ var searchData=
   ['graph_20algorithms',['Graph Algorithms',['../group__graph__algorithms.html',1,'']]],
   ['greater_5fequal_5fvalue',['greater_equal_value',['../structcusp_1_1greater__equal__value.html',1,'cusp']]],
   ['greater_5fvalue',['greater_value',['../structcusp_1_1greater__value.html',1,'cusp']]],
-  ['grid_2eh',['grid.h',['../grid_8h.html',1,'']]]
+  ['grid_2eh',['grid.h',['../grid_8h.html',1,'']]],
+  ['grid2d',['grid2d',['../group__gallery.html#ga54b2b9e9c00bbf3d50fdf2ea2065c8f7',1,'cusp::gallery']]],
+  ['grid3d',['grid3d',['../group__gallery.html#gacbb9c1578c6e185b5996bc682e9b8495',1,'cusp::gallery']]]
 ];
--- search/groups_5.js
+++ search/groups_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['gallery',['Gallery',['../group__gallery.html',1,'']]],
   ['graph_20algorithms',['Graph Algorithms',['../group__graph__algorithms.html',1,'']]]
 ];
